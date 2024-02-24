1fbab6b0bc499cf2c18838c4fca82db49da657de
da33git <https://da33git@github.com>
2024-02-24 00:41:19 -0500

    feat: add options to fetch calls

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,8 @@
 export const handler = async (event, context) => {
-  console.log("Remaining time: ", context.getRemainingTimeInMillis());
-  console.log("Function name: ", context.functionName);
-  console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2));
-  console.info("EVENT\n" + JSON.stringify(event, null, 2));
+  //   console.log("Remaining time: ", context.getRemainingTimeInMillis());
+  //   console.log("Function name: ", context.functionName);
+  //   console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2));
+  //   console.info("EVENT\n" + JSON.stringify(event, null, 2));
 
   return {
     statusCode: 200,
